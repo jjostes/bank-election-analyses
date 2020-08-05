d2062c312e31d479ca2673d52d50d217a47400a4
jjostes <jjostes@protonmail.com>
2020-08-05 16:51:37 -0500

    edits made to PyBank.py

--- PyBank.py
+++ PyBank.py
@@ -10,54 +10,68 @@ with open('Resources/budget_data.csv') as budget_csv:
 
     months = 0
     total = 0
+
+    # variable to be used in determining average changes. Takes values of column 2 ('Profits/Losses')
+    col2_list = []
+
     profit_loss = []
+
+    
 
     for row in csvreader:
 
+        # numbers are formatted as strings. Changes them into integers
         row[1] = int(row[1])
 
-        print(row[1])
-
-        #Total number of months
+        # total number of months included in dataset
         months = months + 1
 
-#         #Net total
-#         total = total + row[1]
+        # net total amount of "Profits/Losses" over entire period
+        total = total + row[1]
 
-#         #Average (limiting decimal to 2 places will not work for numbers <> 8 figures)
-#         getcontext().prec = 8
-#         average = Decimal(total) / Decimal(months)
-
-#         #Max/Min
-#         profit_loss.append((row[0], row[1]))
-
-#     profit_loss = sorted(profit_loss, key=lambda profit_loss: profit_loss[1])
-
-#     max1 = profit_loss[-1]
-#     min1 = profit_loss[0]
-
-#     #Print to terminal
-#     print(f"""
-#     John's Financial Analysis
-#     ------------------------------------
-#     total months: {months}
-#     net total: ${total}
-#     average change: ${average}
-#     greatest profit: {max1[0]} ${max1[1]}
-#     greatest loss: {min1[0]} ${min1[1]}
-#         """)
+        # creates a list of all values in "Profits/Losses" column. to be used in finding average changes
+        col2_list.append(row[1])
 
 
-# #Export to text file
+    temp_val = 0
 
-# file = open('budge_file.txt', 'w')
+    # average of the changes in "Profits/Losses" over entire period
+    for x in range(len(col2_list) - 1):
 
-# file.write('Financial Analysis\n')
-# file.write('------------------------------------\n')
-# file.write(f'total months: {months}\n')
-# file.write(f'net total: ${total}\n')
-# file.write(f'average change: ${average}\n')
-# file.write(f'greatest profit: {max1[0]} ${max1[1]}\n')
-# file.write(f'greatest loss: {min1[0]} ${min1[1]}\n')
+        diff_val = col2_list[x+1] - col2_list[x]
+        temp_val += diff_val
 
-# file.close()
+    prof_loss = round(temp_val/months, 2)
+
+
+
+    # min/max
+    col2_sorted = sorted(col2_list)
+    min1 = col2_sorted[0]
+    max1 = col2_sorted[-1]
+
+    #Print to terminal
+    print(f"""
+    John's Financial Analysis
+    ------------------------------------
+    total months: {months}
+    net total: ${total}
+    average change: ${prof_loss}
+    greatest profit: ${max1[1]}
+    greatest loss: ${min1[1]}
+        """)
+
+
+#Export to text file
+
+file = open('budge_file.txt', 'w')
+
+file.write('Financial Analysis\n')
+file.write('------------------------------------\n')
+file.write(f'total months: {months}\n')
+file.write(f'net total: ${total}\n')
+file.write(f'average change: ${prof_loss}\n')
+file.write(f'greatest profit: ${max1[1]}\n')
+file.write(f'greatest loss: ${min1[1]}\n')
+
+file.close()
